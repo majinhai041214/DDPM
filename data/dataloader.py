d2eb45f0844ff1d092a6d80189059accb93305c0
马锦海 <13716391+ma-jinhai@user.noreply.gitee.com>
2025-08-15 11:01:25 +0800

    dataloader

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,1 +1,46 @@
-def build_train_dataloader()
+from datasets import UnpairedDataset
+from torch.utils.data import DataLoader
+import yaml
+import torch
+def build_train_dataloader(cfg):
+    """
+    cfg 应该包含 data 相关字段：
+      - root, ki67_dir, he_dir, ki67_mask_dir
+      - batch, workers, H_size, W_size
+    """
+
+    c = cfg['data']
+    dataset = UnpairedDataset(
+        root = c['root'],
+        ki67_dir=c.get('ki67_dir', 'Ki67'),
+        he_dir=c.get('he_dir', 'HE'),
+        ki67_mask_dir=c.get('ki67_mask_dir', 'mask'),
+        isSSIM=c.get('use_mask', True),
+        H_size=c.get('H_size', 256),
+        W_size=c.get('W_size', 256)
+    )
+
+
+    return DataLoader(
+        dataset,
+        batch_size=c.get('batch',4),
+        shuffle=False,
+        num_workers=c.get('workers',0),
+        pin_memory=True
+    )
+
+def test_dataloader(dataloader, visualize: bool = True):
+    print("🚀 正在测试 DataLoader...")
+    batch = next(iter(dataloader))
+    for key, value in batch.items():
+        if isinstance(value, torch.Tensor):
+            print(f"{key}: {value.shape}")
+        else:
+            print(f"{key}: {type(value)}")
+
+
+if __name__ == "__main__":
+    cfg = yaml.safe_load(open("../configs/train_config.yaml"))
+    train_dataloader = build_train_dataloader(cfg)
+    print(cfg['data'].get('batch'))
+    test_dataloader(train_dataloader)
